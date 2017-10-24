f116059b3eb4dac3a2f7cdc4a992f255c5db93a6
LittleBeazle <zakito8004@gmail.com>
2017-10-23 20:06:29 -0400

    12

--- Code/Chapter4Labs/ManageAccounts.java
+++ Code/Chapter4Labs/ManageAccounts.java
@@ -15,20 +15,30 @@ public class ManageAccounts
     acct1 = new Account(1000, "Sally", 1111);
 
     //create account2 for Joe with $500
+    acct2 = new Account(500, "Joe", 2222);
 
     //deposit $100 to Joe's account
+    acct2.deposit(100);
 
     //print Joe's new balance (use getBalance())
+    System.out.println(acct2.getBalance());
 
     //withdraw $50 from Sally's account
+    acct1.withdraw(50);
 
     //print Sally's new balance (use getBalance())
+    System.out.println(acct1.getBalance());
 
     //charge fees to both accounts
+    acct1.chargeFee();
+    acct2.chargeFee();
 
     //change the name on Joe's account to Joseph
+    acct2.changeName("Joseph");
 
     //print summary for both accounts
+    System.out.println(acct1.toString());
+    System.out.println(acct2.toString());
 
   } // end of main
 } // end of class
