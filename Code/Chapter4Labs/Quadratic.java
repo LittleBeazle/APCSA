691eff5431735fba9a4e3da38d568aa36950c239
LittleBeazle <zakito8004@gmail.com>
2017-10-26 10:19:45 -0400

    2

---
+++ Code/Chapter4Labs/Quadratic.java
@@ -0,0 +1,31 @@
+import java.lang.*;
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Quadratic {
+  public static void main(String[] args) {
+    double a, b, c, discriminant, outPos, outNeg;
+    Scanner scan = new Scanner(System.in);
+    DecimalFormat fmt = new DecimalFormat("##.###");
+
+    System.out.println("A: ");
+    a = scan.nextDouble();
+    System.out.println("B: ");
+    b = scan.nextDouble();
+    System.out.println("C: ");
+    c = scan.nextDouble();
+
+    discriminant = Math.pow(b, 2) - (4 * a * c);
+
+    if (discriminant < 0) {
+      System.out.println("The roots determined from this equation will be an imaginary number!");
+    }
+    else  {
+    outPos = ((0 - b) + Math.sqrt(discriminant)) / (2 * a);
+    outNeg = ((0 - b) - Math.sqrt(discriminant)) / (2 * a);
+
+    System.out.println("The roots are " + fmt.format(outPos) + " and " + fmt.format(outNeg));
+    }
+
+} // main
+} // class
