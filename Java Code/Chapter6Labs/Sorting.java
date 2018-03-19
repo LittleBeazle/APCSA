0fb9a20af0b4933289ab01357b4ea3f5bfa61542
LittleBeazle <zakito8004@gmail.com>
2018-03-19 09:12:01 -0400

    jnk

--- Java Code/Chapter6Labs/Sorting.java
+++ Java Code/Chapter6Labs/Sorting.java
@@ -6,7 +6,7 @@ public class Sorting   {
 
      public static Scanner inval = new Scanner(System.in);
      public static Random rng = new Random();
-     public static final int ARRAYSIZE = 500000;
+     public static final int ARRAYSIZE = 2000;
      public static int[] intlist = new int[ARRAYSIZE];
 
      public static void SelectionSort(int[] arr)   {
@@ -15,6 +15,7 @@ public class Sorting   {
           long sTime = System.currentTimeMillis();
           for (int i = 0; i < arr.length; i++) {
                minIndex = i;
+
                for (int j = i + 1; j < arr.length; j++) {
                     if (arr[j] < arr[minIndex])
                          minIndex = j;
@@ -26,27 +27,56 @@ public class Sorting   {
                }
           }
           long totTime = (System.currentTimeMillis() - sTime) / 1000;
-          System.out.println("Process took " + totTime + " seconds");
+          System.out.println("Selection sort process took " + totTime + " seconds");
      }
 
-     public static int LinearSearch(int[] arr, int x)   {
+     public static void LinearSearch(int[] arr, int x)   {
+          int count1 = 0;
+          int count2 = 0;
+          int monoIndex = 0;
+
           for (int i = 0; i < arr.length; i++) {
-               if (arr[i] == x) return i;
+               if (arr[i] == x) {
+                    count1++;
+                    monoIndex = i;
+               }
           }
-          return -1;
+
+          if (count1 > 1) {
+               int[] indLis = new int[count1];
+               for (int n = 0; n < intlist.length; n++) {
+                    if (intlist[n] == x) {
+                         indLis[count2] = n;
+                         count2++;
+                    }
+               }
+               System.out.print("Search term was found at indexes ");
+               for (int y = 0; y < indLis.length - 1; y++) {
+                    System.out.print(indLis[y] + " ");
+               }
+               System.out.println("and " + indLis[count1 - 1]);
+          }
+          else if (count1 == 1) {
+               System.out.println("Search term found at index " + monoIndex);
+          }
+
+          else System.out.println("Search term was not generated.");
      }
 
-     public static int BinarySearch(int[] arr, int x, int min, int max)   {
-          int mid = ((min + (max - 1)) / 2);
-          if (max >= 1) {
+     public static int BinarySearch(int[] arr, int x)   {
+          int min = 0;
+          int max = arr.length - 1;
+
+          while (min <= max) {
+               int mid = (min + max) / 2;
                if (intlist[mid] == x)
                     return mid;
-               if (intlist[mid] > x) // x left of mid
-                    return BinarySearch(arr, x, min, (mid - 1));
-               else if (intlist[mid] < x)// x right of mid
-                    return BinarySearch(arr, x, (mid + 1), max);
-               }
-          return (-1);
+               if (intlist[mid] > x) // x less than mid
+                    max = mid - 1;
+               else if (intlist[mid] < x)// x greater than mid
+                    min = mid + 1;
+          }
+          return -1;
      }
 
      public static int menu()   {
@@ -78,14 +108,12 @@ public class Sorting   {
                     case 3:
                          System.out.println("Enter the number you'd like to search for: ");
                          int selLin = inval.nextInt();
-                         int linVal = LinearSearch(intlist, selLin);
-                         if (linVal == -1) System.out.println("Input was not generated.");
-                         else System.out.println("Input was found at index " + linVal + " after ");
+                         LinearSearch(intlist, selLin);
                          break;
                     case 4:
                          System.out.println("Enter the number you'd like to search for: ");
                          int selBin = inval.nextInt();
-                         int binVal = BinarySearch(intlist, selBin, 0, intlist.length - 1);
+                         int binVal = BinarySearch(intlist, selBin);
                          if (binVal == -1) System.out.println("Input was not generated.");
                          else System.out.println("Input was found at index " + binVal);
                          break;
